867b98330eb02b0d341ba2b5448b6e6f5d05c05a
israel <yehuda.n.klein@gmail.com>
2023-01-03 17:32:33 +0200

    Making the joystick xml inflatable

--- app/src/main/java/com/example/TopDownShooter/classes/UI/JoystickView.java
+++ app/src/main/java/com/example/TopDownShooter/classes/UI/JoystickView.java
@@ -1,7 +1,9 @@
 package com.example.TopDownShooter.classes.UI;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.PixelFormat;
 import android.util.AttributeSet;
@@ -13,85 +15,71 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
-public class JoystickView extends SurfaceView implements SurfaceHolder.Callback, View.OnTouchListener {
+import com.example.TopDownShooter.R;
 
-    private final float BASE_TOP_RATIO = 0.5f;
+public class JoystickView extends View{
 
+    private static final float TOP_BASE_RATIO = 0.5f;
+    private static final int defaultSize = 50;
+    private int size;// The edge of the "box" that encapsulates the joystick. Equivalent to the min of view's width and height
     private float baseX;
     private float baseY;
     private float topX;
     private float topY;
     private float baseRadius;
     private float topRadius;
+    private Paint basePaint;
+    private Paint topPaint;
 
-    private float baseTopRatio;// The size ratio between the base and the top of the joystick
+    private float topBaseRatio;// The size ratio between the base and the top of the joystick
 
 
     public JoystickView(Context context) {
         super(context);
-
-        this.baseTopRatio = BASE_TOP_RATIO;
-
-        getHolder().addCallback(this);
-        setOnTouchListener(this);
-
-
+        initJoystickView();
     }
 
     public JoystickView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        parseAttributes(attrs);
+        initJoystickView();
 
-        this.baseTopRatio = BASE_TOP_RATIO;
+    }
 
-        getHolder().addCallback(this);
-        setOnTouchListener(this);
+    @Override
+    public void onFinishInflate(){
+        super.onFinishInflate();
 
     }
 
 
     @Override
-    public void surfaceCreated(@NonNull SurfaceHolder surfaceHolder) {
+    public void onDraw(Canvas canvas) {
+
+       // Draw base
+        canvas.drawCircle(baseX, baseY, baseRadius, basePaint);
+
+      // Draw paint         
+        canvas.drawCircle(topX, topY, topRadius, topPaint);
+
+    }
+
+    @Override
+    protected void onMeasure (int widthMeasureSpec, int heightMeasureSpec){
+
+        // Setting the size to be the shortest from the width and height spec, by convention
+        size = Math.min(measure(widthMeasureSpec), measure(heightMeasureSpec));
+        setMeasuredDimension(size, size);
         setUpDimensions();
-        getHolder().setFormat(PixelFormat.TRANSPARENT);
-        // Getting drawn to the screen
-        Canvas canvas = getHolder().lockCanvas();
-        draw(canvas);
-        getHolder().unlockCanvasAndPost(canvas);
-
-
-
-
     }
 
     @Override
-    public void surfaceChanged(@NonNull SurfaceHolder surfaceHolder, int i, int i1, int i2) {
-
+    protected void onSizeChanged(int width, int height, int oldWidth, int oldHeight) {
+        super.onSizeChanged(width, height, oldWidth, oldHeight);
+        size = Math.min(measure(width), measure(height));
     }
 
     @Override
-    public void surfaceDestroyed(@NonNull SurfaceHolder surfaceHolder) {
-
-    }
-
-    @Override
-    public void draw(Canvas canvas) {
-        super.draw(canvas);
-
-        Paint paint = new Paint();
-
-        //Draw base
-        paint.setARGB(255, 50, 100, 50);
-        canvas.drawCircle(baseX, baseY, baseRadius, paint);
-
-        // Draw top
-        paint.setARGB(255, 0, 0, 255);
-        canvas.drawCircle(topX, topY, topRadius, paint);
-
-    }
-
-    @Override
-    public boolean onTouch(View view, MotionEvent motionEvent) {
+    public boolean onTouchEvent(MotionEvent motionEvent) {
         switch(motionEvent.getAction()){
             case MotionEvent.ACTION_UP:
                 resetJoystick();
@@ -102,6 +90,8 @@ public class JoystickView extends SurfaceView implements SurfaceHolder.Callback,
         }
         return true;
     }
+
+
 
     // Method that returns the actuators in each direction in order to use the joy stick for any purpose
     public float getActuatorX(){
@@ -114,18 +104,49 @@ public class JoystickView extends SurfaceView implements SurfaceHolder.Callback,
         return distanceY/baseRadius;
     }
 
+    private void initJoystickView(){
+        this.topBaseRatio = TOP_BASE_RATIO;
+
+        basePaint = new Paint();
+        basePaint.setColor(Color.RED);
+
+        topPaint = new Paint();
+        topPaint.setColor(Color.GRAY);
+    }
+
+    // Method for generating a fixed size from the width/height specification
+    private int measure(int measureSpec){
+
+        // Decode the measurement specifications.
+        int specMode = MeasureSpec.getMode(measureSpec);
+        int specSize = MeasureSpec.getSize(measureSpec);
+
+        if (specMode == MeasureSpec.UNSPECIFIED) {
+            // Return a default size of 50 if no bounds are specified.
+            return defaultSize;
+        }
+        else {
+
+            return specSize;
+        }
+
+    }
+
     // Calculate the initial position for the base and the radius of the size of the joystick
     private void setUpDimensions(){
-        baseX = getWidth()/2;
-        baseY = getHeight()/2;
+        baseX = size/2;
+        baseY = size/2;
+
         topX = baseX;
         topY = baseY;
 
         // Make sure that the joystick will not go outbound
-        float radius = Math.min(getWidth(), getHeight())/2;
-        baseRadius = radius;
-        topRadius = baseRadius * baseTopRatio;
+        // The formula for the calculation of the radius was found using algebra on paper
+        // (1) topRadius + baseRadius == size/2
+        // (2) topRadius/baseRadius == topBaseRatio
 
+        topRadius = ((size * topBaseRatio)/(2 + 2 *topBaseRatio));
+        baseRadius = size/2 - topRadius;
 
     }
 
@@ -148,40 +169,24 @@ public class JoystickView extends SurfaceView implements SurfaceHolder.Callback,
             topX = positionX;
             topY = positionY;
         }
-
-        updateJoystick();
+        invalidate();
     }
 
     // Return the joystick top to the middle and redraw the joystick and render the joystick
     private void resetJoystick(){
         topX = baseX;
         topY = baseY;
-
-        updateJoystick();
-
+        invalidate();
     }
 
     // Re-render the joystick again according to the values of topX and topY
-    private boolean updateJoystick(){
-        Canvas canvas = getHolder().lockCanvas();
 
-        if(canvas == null){ return false;}
-
-        draw(canvas);
-        getHolder().unlockCanvasAndPost(canvas);
-
-        return true;
-
-
-    }
 
     private float distance(float x1, float y1, float x2, float y2){
         return (float)Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
     }
 
     private void parseAttributes(AttributeSet attrs) {
-
-
 
     }
 
